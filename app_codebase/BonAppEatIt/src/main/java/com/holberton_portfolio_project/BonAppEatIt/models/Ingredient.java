790695bed7e40e3816f640cb0de4dd5d59057200
jmitchell35 <julian.m.mitchell@gmail.com>
2025-06-22 11:30:41 +0200

    Adding last two entities

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/models/Ingredient.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/models/Ingredient.java
@@ -1,12 +1,18 @@
 package com.holberton_portfolio_project.BonAppEatIt.models;
 
 // jakarta.persistence
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 
 
 // org.springframework.data
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 // Lombok boilerplate methods
@@ -16,10 +22,61 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Builder
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name = "ingredient_id"))
+@Table(name = "ingredients")
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Ingredient extends BaseEntity {}
+public class Ingredient extends BaseEntity {
+    @Column(name = "ingredient_singular")
+    private String ingredientSingular;
+
+    @Column(name = "ingredient_plural")
+    private String ingredientPlural;
+
+    @Column(name = "available_in_january")
+    private boolean availableInJanuary;
+
+    @Column(name = "available_in_february")
+    private boolean availableInFebruary;
+
+    @Column(name = "available_in_march")
+    private boolean availableInMarch;
+
+    @Column(name = "available_in_april")
+    private boolean availableInApril;
+
+    @Column(name = "available_in_may")
+    private boolean availableInMay;
+
+    @Column(name = "available_in_june")
+    private  boolean availableInJune;
+
+    @Column(name = "available_in_july")
+    private boolean availableInJuly;
+
+    @Column(name = "available_in_august")
+    private boolean availableInAugust;
+
+    @Column(name = "available_in_september")
+    private boolean availableInSeptember;
+
+    @Column(name = "available_in_october")
+    private boolean availableInOctober;
+
+    @Column(name = "available_in_november")
+    private boolean availableInNovember;
+
+    @Column(name = "available_in_december")
+    private boolean availableInDecember;
+
+    @OneToMany(targetEntity = RecipeIngredient.class, mappedBy = "ingredient", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<RecipeIngredient> recipeIngredients = new HashSet<>();
+}
--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/models/Unit.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/models/Unit.java
@@ -1,4 +1,37 @@
 package com.holberton_portfolio_project.BonAppEatIt.models;
 
-public class Unit {
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name = "unit_id"))
+@Table(name = "units")
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Unit extends BaseEntity {
+
+    @OneToMany(targetEntity = RecipeIngredient.class, mappedBy = "unit", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<RecipeIngredient> recipeIngredients = new HashSet<>();
+
+    @Column(name = "name")
+    private String name;
 }
