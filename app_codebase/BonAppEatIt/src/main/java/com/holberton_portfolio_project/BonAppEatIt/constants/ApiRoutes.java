6018caeb8253565258abb431d69489f1fd6a20b2
jmitchell35 <julian.m.mitchell@gmail.com>
2025-06-30 13:24:12 +0200

    Adding ApiRoutes file as some kind of Blueprint reference

---
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/constants/ApiRoutes.java
@@ -0,0 +1,11 @@
+package com.holberton_portfolio_project.BonAppEatIt.constants;
+
+public class ApiRoutes {
+    public static class V1 {
+        public static final String BASE = "/api/v1";
+        public static final String AUTH = BASE + "/auth";
+        public static final String ADMIN = BASE + "/admin";
+        public static final String RECIPES = BASE + "/recipes";
+        
+    }
+}
