71c2ad5176c0ad3cf232f0da880cd532ff00da47
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-03 14:23:43 +0200

    Removing unused Role data model import

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UserLightDTO.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UserLightDTO.java
@@ -1,6 +1,5 @@
 package com.holberton_portfolio_project.BonAppEatIt.dto;
 
-import com.holberton_portfolio_project.BonAppEatIt.models.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
