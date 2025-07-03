acec7c0f287a6a2fda207eb39124129362d7e3a4
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-03 15:57:59 +0200

    Adding UserLoginDTO

---
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UserLoginDTO.java
@@ -0,0 +1,14 @@
+package com.holberton_portfolio_project.BonAppEatIt.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public class UserLoginDTO {
+    @Email(message = "Please enter a valid email address")
+    @NotEmpty(message = "Please enter the email address you registered with")
+    private String email;
+
+    @NotEmpty(message = "Please enter your password")
+    private String password;
+}
