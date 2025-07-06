0cc95801fbb6f5ba0d4b7f5769eb03f50e6e62c0
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-06 19:09:46 +0200

    Added the @builder

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UserCreatedDTO.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UserCreatedDTO.java
@@ -1,11 +1,13 @@
 package com.holberton_portfolio_project.BonAppEatIt.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 public class UserCreatedDTO {
     private String email;
