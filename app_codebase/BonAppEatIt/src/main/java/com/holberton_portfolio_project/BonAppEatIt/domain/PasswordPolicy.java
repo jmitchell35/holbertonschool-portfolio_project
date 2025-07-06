0ad1ab41a41cc18a8ad725d56ef8f568e31b60d7
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-06 16:17:26 +0200

    Password configuration requires the PasswordPolicy class

---
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/domain/PasswordPolicy.java
@@ -0,0 +1,12 @@
+package com.holberton_portfolio_project.BonAppEatIt.domain;
+
+import lombok.Builder;
+
+@Builder
+public class PasswordPolicy {
+    private int minLength;
+    private boolean requireUppercase;
+    private boolean requireLowercase;
+    private boolean requireNumbers;
+    private boolean requireSpecialChars;
+}
