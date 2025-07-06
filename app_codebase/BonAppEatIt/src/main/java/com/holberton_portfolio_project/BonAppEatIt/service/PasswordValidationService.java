feb503784dbc41fd3d23ac678727966d7a0482ce
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-06 17:12:02 +0200

    Basic core for the Password Validation service

---
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/service/PasswordValidationService.java
@@ -0,0 +1,15 @@
+package com.holberton_portfolio_project.BonAppEatIt.service;
+
+import com.holberton_portfolio_project.BonAppEatIt.domain.PasswordPolicy;
+import com.holberton_portfolio_project.BonAppEatIt.exceptions.WeakPasswordException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordValidationService {
+
+    public void validatePassword(String password, PasswordPolicy policy) {
+        if (password.length() < policy.getMinLength()) {
+            throw new WeakPasswordException("Password must be at least " + policy.getMinLength() + " characters long");
+        }
+    }
+}
