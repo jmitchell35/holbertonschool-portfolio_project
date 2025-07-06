00829a9a11875c5d4cc68f0077b7d8045f7a5fe4
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-06 19:09:08 +0200

    Withdrew former methods now handled by the AuthService, removed unused import statements

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/service/UserService.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/service/UserService.java
@@ -1,15 +1,11 @@
 package com.holberton_portfolio_project.BonAppEatIt.service;
 
+import com.holberton_portfolio_project.BonAppEatIt.domain.HashedPassword;
 import com.holberton_portfolio_project.BonAppEatIt.dto.UserCreatedDTO;
-import com.holberton_portfolio_project.BonAppEatIt.dto.UserCreationDTO;
-import com.holberton_portfolio_project.BonAppEatIt.dto.UserLoginDTO;
-import com.holberton_portfolio_project.BonAppEatIt.exceptions.UserAlreadyExistsException;
+import com.holberton_portfolio_project.BonAppEatIt.models.User;
 import com.holberton_portfolio_project.BonAppEatIt.repository.UserRepository;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @Service
 @AllArgsConstructor
@@ -17,25 +13,21 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserCreatedDTO createUser(UserCreationDTO dto) {
-        if (emailAlreadyExists(dto.getEmail())) {
-            throw new UserAlreadyExistsException("Email is already registered.");
-        }
-        // check password strength (specific method)
-        // hash password
-        // propagate exceptions to controller level for proper HTTP response
-        // create user entity from DTO
-        // write to the db and sync
-        // create UserCreatedDTO from entity
+    public UserCreatedDTO persistUser(String email, HashedPassword password) {
+        User persistedUser = User.builder()
+                .email(email)
+                .password(String.valueOf(password))
+                .build();
+
+        userRepository.save(persistedUser);
+
+        return UserCreatedDTO.builder()
+                .email(email)
+                .build();
     }
 
     // package private (no modifier instead of private)
     boolean emailAlreadyExists(String email) {
         return userRepository.existsByEmail(email);
     }
-
-    private boolean isStrongPassword(String password) {
-
-    }
-
 }
