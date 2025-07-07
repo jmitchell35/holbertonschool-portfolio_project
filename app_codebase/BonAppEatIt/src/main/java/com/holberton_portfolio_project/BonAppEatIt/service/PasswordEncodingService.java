3e374207c966868d91d9581ee0287a031908e5ed
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-07 14:25:31 +0200

    PasswordEncodingService now implements PasswordEncoder interface : can be detected as PasswordEncoder by Spring and used for password matching during authentication.

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/service/PasswordEncodingService.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/service/PasswordEncodingService.java
@@ -1,10 +1,12 @@
 package com.holberton_portfolio_project.BonAppEatIt.service;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PasswordEncodingService {
+public class PasswordEncodingService implements PasswordEncoder {
+
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public  PasswordEncodingService() {
@@ -17,5 +19,20 @@ public class PasswordEncodingService {
 
     public boolean comparePasswords(String rawPassword, String encodedPassword) {
         return bCryptPasswordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+    @Override
+    public boolean upgradeEncoding(String encodedPassword) {
+        return PasswordEncoder.super.upgradeEncoding(encodedPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return comparePasswords(rawPassword.toString(), encodedPassword);
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return hashPassword(rawPassword.toString());
     }
 }
