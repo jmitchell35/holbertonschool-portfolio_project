40ae9f6e73dd4daf80bd5c5eed40f14c922326f4
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-08 12:56:21 +0200

    Modifying ApiRoutes constants accordingly.

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/constants/ApiRoutes.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/constants/ApiRoutes.java
@@ -3,9 +3,8 @@ package com.holberton_portfolio_project.BonAppEatIt.constants;
 public class ApiRoutes {
     public static class V1 {
         public static final String BASE = "/api/v1";
-        public static final String AUTH = BASE + "/auth";
-        public static final String ADMIN = BASE + "/admin";
-        public static final String RECIPES = BASE + "/recipes";
-        
+        public static final String AUTH = V1.BASE + "/auth";
+        public static final String ADMIN = V1.BASE + "/admin";
+        public static final String RECIPES = V1.BASE + "/recipes";
     }
 }
