1fc1a3584993709ecd80a7d5ee663b7afbe63079
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-09 15:17:14 +0200

    Building a UserLoginDTO for future usage

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UserLoginDTO.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UserLoginDTO.java
@@ -3,7 +3,15 @@ package com.holberton_portfolio_project.BonAppEatIt.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserLoginDTO {
     @Email(message = "Please enter a valid email address")
     @NotEmpty(message = "Please enter the email address you registered with")
