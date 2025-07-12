d858403e61399f53aa5e48cfc7fc201d083591dc
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-12 23:01:44 +0200

    Adding the ID for front-side submission

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/IngredientDTO.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/IngredientDTO.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class IngredientDTO {
+    private UUID id;
     private String ingredientSingular;
     private String ingredientPlural;
 }
