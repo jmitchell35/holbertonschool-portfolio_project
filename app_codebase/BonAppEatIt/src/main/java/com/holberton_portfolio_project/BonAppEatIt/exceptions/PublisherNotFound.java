fe7ca046f5babc9fe0f8fbe991a0247ee2375120
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-13 16:29:30 +0200

    Consistency

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/exceptions/PublisherNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.holberton_portfolio_project.BonAppEatIt.exceptions;
-
-public class PublisherNotFound extends RuntimeException {
-    public PublisherNotFound(String message) {
-        super(message);
-    }
-}
---
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/exceptions/PublisherNotFoundException.java
@@ -0,0 +1,7 @@
+package com.holberton_portfolio_project.BonAppEatIt.exceptions;
+
+public class PublisherNotFoundException extends RuntimeException {
+    public PublisherNotFoundException(String message) {
+        super(message);
+    }
+}
