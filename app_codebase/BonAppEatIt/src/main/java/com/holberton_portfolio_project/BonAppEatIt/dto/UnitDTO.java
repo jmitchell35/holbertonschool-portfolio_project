d898bddc3fa9109daa70ee351a97e8644ab22ca2
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-13 17:44:27 +0200

    Adding Getter, constructor, builder

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UnitDTO.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/UnitDTO.java
@@ -1,7 +1,14 @@
 package com.holberton_portfolio_project.BonAppEatIt.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
+@AllArgsConstructor
+@Builder
 public class UnitDTO {
     private UUID id;
     private String name;
