8987d0a4c9610179aff22917274317b3a91bcf64
jmitchell35 <julian.m.mitchell@gmail.com>
2025-07-16 16:58:08 +0200

    Comments about Integer values

--- app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/RecipeFiltersDTO.java
+++ app_codebase/BonAppEatIt/src/main/java/com/holberton_portfolio_project/BonAppEatIt/dto/RecipeFiltersDTO.java
@@ -13,12 +13,13 @@ public class RecipeFiltersDTO {
     private List<String> includeIngredients;    // Must contain these ingredients
     private List<String> excludeIngredients;    // Must NOT contain these
 
+    // Integer rather than int because will default to null instead of 0 if the user doesn't want fo filter
     @Min(value = 2, message = "You can't filter below 2 ingredients")
-    private int maxIngredients;             // Maximum number of ingredients
+    private Integer maxIngredients;             // Maximum number of ingredients
 
     @Min(value = 1, message = "Month must be between 1 and 12")
     @Max(value = 12, message = "Month must be between 1 and 12")
-    private int month;                      // 1-12 for seasonality (current month)
-    private int maxPrepTime;                // Maximum prep time in minutes
-    private List<String> tags;                  // Must have these tags
+    private Integer month;                      // 1-12 for seasonality
+    private Integer maxPrepTime;                // Maximum prep time in minutes
+    private List<String> tags;                  // Must have all tags
 }
