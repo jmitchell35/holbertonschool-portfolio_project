fbe5e43bcd03682ad4f34ae3614b3d8f383b7c78
jmitchell35 <julian.m.mitchell@gmail.com>
2025-09-07 19:31:53 +0200

    Leaning on tests

---
+++ app_codebase/BonAppEatIt/src/test/java/com/holberton_portfolio_project/BonAppEatIt/service/AuthServiceTest.java
@@ -0,0 +1,8 @@
+package com.holberton_portfolio_project.BonAppEatIt.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceTest {
+}
