73d660b9f9f1c81a468144a672e8b3286422d7a9
jmitchell35 <julian.m.mitchell@gmail.com>
2025-09-15 16:30:26 +0000

    Testing user registration

--- app_codebase/BonAppEatIt/src/test/java/com/holberton_portfolio_project/BonAppEatIt/service/AuthServiceTest.java
+++ app_codebase/BonAppEatIt/src/test/java/com/holberton_portfolio_project/BonAppEatIt/service/AuthServiceTest.java
@@ -1,8 +1,66 @@
 package com.holberton_portfolio_project.BonAppEatIt.service;
 
+import com.holberton_portfolio_project.BonAppEatIt.domain.HashedPassword;
+import com.holberton_portfolio_project.BonAppEatIt.domain.PasswordPolicy;
+import com.holberton_portfolio_project.BonAppEatIt.dto.UserCreatedDTO;
+import com.holberton_portfolio_project.BonAppEatIt.dto.UserCreationDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
+    @Mock
+    private UserService mockedUserService;
+
+    @Mock
+    private final PasswordValidationService mockedPasswordValidationService = mock(PasswordValidationService.class);
+
+    @Mock
+    private final PasswordEncodingService mockedPasswordEncodingService = mock(PasswordEncodingService.class);
+
+    @Mock
+    private final PasswordPolicy mockedStandardPasswordPolicy = mock(PasswordPolicy.class);
+
+    @InjectMocks  // This injects all the mocks above into AuthService
+    private AuthService authService;
+
+    @Test
+    void shouldRegisterNewUser() {
+        // Given => provide test data
+        UserCreationDTO dto = new UserCreationDTO(
+                "test@domain.de",
+                "test@domain.de",
+                "testuser35",
+                "P@ssword12345",
+                "P@ssword12345"
+        );
+
+        // When => mock behavior
+        when(mockedUserService.emailAlreadyExists(dto.getEmail())).thenReturn(false);
+        when(mockedUserService.usernameAlreadyExists(dto.getUsername())).thenReturn(false);
+        doNothing()
+                .when(mockedPasswordValidationService)
+                .validatePassword(dto.getPassword(), mockedStandardPasswordPolicy);
+        when(mockedPasswordEncodingService.hashPassword(dto.getPassword()))
+                .thenReturn("$2a$12$R94jzkiBohNSUQM4vua3kOC5Z7k2gnuV5n8tedUnV5hkVNgpByWP6");
+        when(mockedUserService.createUser(
+                eq(dto.getEmail()),
+                eq(dto.getUsername()),
+                any(HashedPassword.class)
+        )).thenReturn(UserCreatedDTO.builder()
+                        .username(dto.getUsername())
+                        .email(dto.getEmail())
+                        .build());
+
+        UserCreatedDTO result = authService.registerUser(dto);
+
+        Assertions.assertEquals(dto.getUsername(), result.getUsername());
+        Assertions.assertEquals(dto.getEmail(), result.getEmail());
+    }
 }
