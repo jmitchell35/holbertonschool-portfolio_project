9a2da2cdb1ac7a6fc1b02c00892b2c842e214b63
jmitchell35 <julian.m.mitchell@gmail.com>
2025-09-17 15:37:58 +0000

    Testing IngredientMapper, both size and order preservation

---
+++ app_codebase/BonAppEatIt/src/test/java/com/holberton_portfolio_project/BonAppEatIt/mappers/IngredientMapperTest.java
@@ -0,0 +1,91 @@
+package com.holberton_portfolio_project.BonAppEatIt.mappers;
+
+import com.holberton_portfolio_project.BonAppEatIt.dto.IngredientDTO;
+import com.holberton_portfolio_project.BonAppEatIt.models.Ingredient;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class IngredientMapperTest {
+    Ingredient ingredient1 = new Ingredient(
+            "Singular",
+            "Plural",
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true, true,
+            true,
+            true,
+            new HashSet<>()
+    );
+
+    Ingredient ingredient2 = new Ingredient(
+            "Singular2",
+            "Plural2",
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true, true,
+            true,
+            true,
+            new HashSet<>()
+    );
+
+    List<Ingredient> testList = new ArrayList<>();
+
+    IngredientMapper mapper = new IngredientMapper();
+
+    @Test
+    void shouldOutputSimpleOutputDTO() {
+        ReflectionTestUtils.setField(ingredient1, "id", UUID.randomUUID());
+
+        IngredientDTO test = mapper.toOutputDTO(ingredient1);
+
+        assertThat(test)
+                .isNotNull()
+                .isInstanceOf(IngredientDTO.class);
+
+        assertThat(test.getId())
+                .isNotNull()
+                .isEqualTo(ingredient1.getId());
+
+        assertThat(test.getIngredientSingular())
+                .isNotNull()
+                .isEqualTo(ingredient1.getIngredientSingular());
+
+        assertThat(test.getIngredientPlural())
+                .isNotNull()
+                .isEqualTo(ingredient1.getIngredientPlural());
+    }
+
+    @Test
+    void shouldOutputListOfOutputDTO() {
+        testList.add(ingredient1);
+        testList.add(ingredient2);
+
+        ReflectionTestUtils.setField(ingredient1, "id", UUID.randomUUID());
+        ReflectionTestUtils.setField(ingredient2, "id", UUID.randomUUID());
+
+        List<IngredientDTO> test = mapper.toOutputDTO(testList);
+
+        assertThat(test)
+                .isNotNull()
+                .hasSize(2)
+                .extracting(IngredientDTO::getIngredientSingular)
+                .containsExactly(ingredient1.getIngredientSingular(), ingredient2.getIngredientSingular());
+    }
+}
